2c4db5e6b0fe1dc3695ee012d173368c958cc5b3
ABC <hassan.ibn.salih@gmail.com>
2026-08-25 09:23:45 +0100

    Add .vitepress/theme/index.ts

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,8 @@
+import DefaultTheme from 'vitepress/theme'
+import '@fontsource/noto-sans-arabic/400.css'
+import '@fontsource/noto-sans-arabic/500.css'
+import '@fontsource/noto-sans-arabic/600.css'
+import '@fontsource/noto-sans-arabic/700.css'
+import './custom.css'
+
+export default DefaultTheme
